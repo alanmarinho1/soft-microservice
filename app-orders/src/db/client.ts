54eed1d5c35a96e22208b51efcf77dd3d9577107
Alan Marinho <kgometalenator@gmail.com>
2025-06-12 00:13:40 -0300

    add dotenv dependency and configure environment variables

--- app-orders/src/db/client.ts
+++ app-orders/src/db/client.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
 
 if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined')
+  throw new Error("DATABASE_URL is not defined");
 }
 
 export const db = drizzle(process.env.DATABASE_URL, {
-    casing: 'snake_case'
+  casing: "snake_case",
 });
